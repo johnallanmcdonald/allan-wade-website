f255ab672c5bdbac1b33d395d183f6b509b04f20
johnallanmcdonald <jmcdonald@allan-wade.com>
2016-10-31 18:04:39 -0400

    Add files via upload

---
+++ proven-process.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en"> 
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <meta name="description" content="Inbound marketing and lead management performed and delivered by The Allan Wade Company">
+    <meta name="keywords" content="inbound marketing proven process, inbound marketing, proven process">
+    <meta name="robots" content="index, follow">
+    <link rel="canonical" href="http://www.allan-wade.com/proven-process.php"/>
+    <title>Inbound Marketing Proven Process | The Allan Wade Company </title>
+    <?php include 'includes/header.php';?>
+    
+    <div class="container">
+        <div class="jumbotron">
+             <h1>The Allan Wade Company Inbound Marketing Proven Process</h1>
+             <p>7 Steps for Inbound Marketing Nirvana</p>
+             <a href="landing-pages/engage/engage.php" class="btn btn-primary btn-lg">Engage with us today</a>
+        </div>
+    </div>
+    <div class="container">
+        <img class="img-responsive" src="images/allan-wade-proven-process.svg" alt="The Allan Wade Inbound Marketing Proven Process" style="width: 100%;">
+        <h2>Starting Your Inbound Marketing Program</h2>
+        <p class="lead">The 60 Minute Meeting is where we'll begin. We need to get to know each other first.</p>
+        <p>During the first 60 mintutes we'll:</p>
+        <ul>
+            <li>Get to know you</li>
+            <li>You'll get to know us</li>
+            <li>Learn our process</li>
+            <li>Learn our tools</li>
+        </ul>
+        <h2>Getting Focused and Laying the Foundation for Inbound Marketing Success</h2>
+        <p class="lead">We'll work together to focus our efforts and lay the foundation for your success.</p>
+        <p><strong>Getting Focused:</strong> Together we'll review your existing marketing, inbound and outbound styles, effectiveness and resources.  This ensures that we shore up successes and shore up areas that are lacking.</p>
+        <p><strong>Laying the Foundation for Marketing Success:</strong> Pulling out our toolkit we'll develop buyer persona's and journeys, keyword maps, content plans, goals, lead scoring and lead transitioning tools.</p>
+        <h2>Engaging the Inbound Marketing Lead Continuum</h2>
+        <p class="lead">With the ground work laid, success is just a stones throw away.</p>
+        <p>We engage the lead-continuum. The lead-continuum engages the Quarterly Rocks, monthly objects and weekly tasks that, when executed, result in inbound leads.</p>
+    </div>
+    <div class="container-fluid" style="background-color: white; text-align: center; padding-bottom: 45px; padding-top: 45px; border-top: solid 1px silver;">
+      <h2 style="color: black;">Ready to discuss solutions?</h2>
+      <a href="landing-pages/lets-talk/lets-talk.php" class="btn btn-warning btn-lg">Let's talk solutions!</a>
+    </div>
+    
+
+    <?php include 'includes/footer.php';?>
+
+  </body>
+</html>
